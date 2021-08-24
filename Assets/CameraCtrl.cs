a6c60121bfa56eb696906f67d808e637c8407389
Khaos <okh8609@gmail.com>
2021-08-24 11:25:53 +0800

    相機跟隨

---
+++ Assets/CameraCtrl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = this.transform.position - target.transform.position;
+        //transform.parent = null;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    Vector3 target_pos = target.transform.position + offset;
+    //    this.transform.position = Vector3.Lerp(this.transform.position, target_pos, 3.0f * Time.deltaTime);
+    //}
+
+    private void FixedUpdate()
+    {
+        Vector3 target_pos = target.transform.position + offset;
+        this.transform.position = Vector3.Lerp(this.transform.position, target_pos, 3.0f * Time.fixedDeltaTime);
+    }
+}
